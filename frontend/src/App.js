c214a3fcc2494e3bd51068d6d65def6600934ff3
Madhumitha <madhumithavenu03@gmail.com>
2023-11-09 13:19:18 +0530

    Added code for header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,19 @@ import Products from './components/products/Products.jsx';
 import SignUp from './components/signup/SignUp.jsx';
 import Cart from './components/cart/Cart.jsx';
 import Header from './components/header/Header.jsx'
-//import { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  //const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   return (
     <BrowserRouter>
-      <Header cartItems={null} />
+      <Header cartItems={cartItems} />
       <Routes>
-        <Route exact path="/" element={<Products />} />
+        <Route exact path="/" element={<Products/>} />
         <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/cart" element={<Cart cartItems={cartItems}/>} />
       </Routes>
     </BrowserRouter>
   );
