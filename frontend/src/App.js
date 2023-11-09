d62cd048bd60bc5a7c51fdc5b70e1804a138e36b
Madhumitha <madhumithavenu03@gmail.com>
2023-11-09 12:35:51 +0530

    Added code for components, react router dom working sucessfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Products from './components/products/Products.jsx';
+import SignUp from './components/signup/SignUp.jsx';
+import Cart from './components/cart/Cart.jsx';
+import Header from './components/header/Header.jsx'
+//import { useState } from 'react';
 
 function App() {
 
-  const [cartItems, setCartItems] = useState([]);
+  //const [cartItems, setCartItems] = useState([]);
 
   return (
     <BrowserRouter>
-      <Header cartItems={cartItems} />
+      <Header cartItems={null} />
       <Routes>
         <Route exact path="/" element={<Products />} />
         <Route exact path="/signup" element={<SignUp />} />
