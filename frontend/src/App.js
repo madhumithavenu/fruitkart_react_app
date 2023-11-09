a1fcf6c94cc0ce03d630c39cf8a8932dd734aad4
Madhumitha <madhumithavenu03@gmail.com>
2023-11-09 12:20:17 +0530

    Added images and created component files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([]);
+
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <Header cartItems={cartItems} />
+      <Routes>
+        <Route exact path="/" element={<Products />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
